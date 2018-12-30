362a9f4ecef174be14f35c7f980733018a7b1ad1
ixpert <amr.elnagar@outlook.com>
2018-12-30 12:03:58 +0100

    Initial commit

---
+++ src/main/java/com/ixpert/countryandstate/CountryandstateApplication.java
@@ -0,0 +1,14 @@
+package com.ixpert.countryandstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryandstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryandstateApplication.class, args);
+    }
+
+}
+
